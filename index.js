ea34deeec88d42d35f5456fc773e6eaa44f1b151
Chris Gruber <chris@chrisgruber.com>
2017-08-13 02:10:31 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const jwt = require("jwt-simple");
+const auth = require("./auth.js")();
+const users = require("./users.js");
+const cfg = require("./config.js");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(auth.initialize());
+
+app.get("/", function(req, res) {
+    res.json({
+        status: "Hello, World!"
+    });
+});
+
+app.get("/user", auth.authenticate(), function(req, res) {
+    console.log(JSON.stringify(users));
+    res.json(users[req.user.id]);
+});
+
+app.post("/token", function(req, res) {
+    if (req.body.email && req.body.password) {
+        var email = req.body.email;
+        var password = req.body.password;
+        var user = users.find(function(u) {
+            return u.email === email && u.password === password;
+        });
+        if (user) {
+            var payload = {
+                id: user.id
+            };
+            var token = jwt.encode(payload, cfg.jwtSecret);
+            res.json({
+                token: token
+            });
+        } else {
+            res.sendStatus(401);
+        }
+    } else {
+        res.sendStatus(401);
+    }
+});
+
+app.listen(3000, function() {
+    console.log("ExpressPassportJwtExample Api listening on port 3000...");
+});
+
+module.exports = app;
---
+++ users.js
@@ -0,0 +1,14 @@
+// Fake list of users to be used in the authentication
+var users = [{
+    id: 1,
+    name: "chris",
+    email: "chris@chrisgruber.com",
+    password: "chris"
+}, {
+    id: 2,
+    name: "Sarah",
+    email: "sarah@mail.com",
+    password: "sarah123"
+}];
+
+module.exports = users;
